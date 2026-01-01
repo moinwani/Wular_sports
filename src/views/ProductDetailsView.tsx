d5013afff6336f8197b4b6ada9ed5770bd5d0855
moinwani <moinwani91@gmail.com>
2026-01-01 13:39:02 +0530

    Fix: Remove unused React imports in ProductDetailsView

--- src/views/ProductDetailsView.tsx
+++ src/views/ProductDetailsView.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react';
+import { FC, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { products } from '../data/products';
 import { ProductFull } from '../types';

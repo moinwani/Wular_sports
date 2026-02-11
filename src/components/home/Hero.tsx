8ea435d47097fc24660933d5b1697a4939bce6fd
moinwani <moinwani91@gmail.com>
2026-02-11 21:00:48 +0530

    Update mobile hero slider to use 5 new uploaded images

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -7,10 +7,11 @@ export interface HeroProps {
 const DESKTOP_IMAGE = 'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/desktop/hero-1.png';
 
 const MOBILE_IMAGES = [
-    'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/mobile/hero-1.jpg',
-    'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/mobile/hero-2.jpg',
-    'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/mobile/hero-3.jpg',
-    'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/mobile/hero-4.PNG',
+    'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/mobile/hero-1.png',
+    'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/mobile/hero-2.png',
+    'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/mobile/hero-3.PNG',
+    'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/mobile/hero-4.png',
+    'https://cdn.jsdelivr.net/gh/moinwani/Wular_sports@main/assets/images/hero/mobile/hero-5.png',
 ];
 
 export const Hero: FC<HeroProps> = memo(({ onShopCollectionClick }) => {
